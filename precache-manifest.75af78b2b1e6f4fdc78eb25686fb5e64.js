cf04a40361b02a402c8dcd69279421960c98abdb
seyha phan <phan.seiha2017@gmail.com>
2020-07-06 15:42:25 +0700

    Updates

---
+++ precache-manifest.75af78b2b1e6f4fdc78eb25686fb5e64.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8351c879f20e6d0cc84f92ff5b951c80",
+    "url": "/covid19kh/index.html"
+  },
+  {
+    "revision": "05e05217856f8ceeefb9",
+    "url": "/covid19kh/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "8b07028ac6925ee26b83",
+    "url": "/covid19kh/static/css/main.ae314ca0.chunk.css"
+  },
+  {
+    "revision": "05e05217856f8ceeefb9",
+    "url": "/covid19kh/static/js/2.79d36285.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covid19kh/static/js/2.79d36285.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b07028ac6925ee26b83",
+    "url": "/covid19kh/static/js/main.533546d4.chunk.js"
+  },
+  {
+    "revision": "316a3e2533650f159303",
+    "url": "/covid19kh/static/js/runtime-main.fc388b71.js"
+  }
+]);
